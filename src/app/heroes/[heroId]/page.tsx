04db48163706749e7012d7fcafed22ca78df5f05
marcobcchen <marco8854144@gmail.com>
2026-04-09 01:15:59 +0800

    feat: enhance loading and error messages in HeroPage component

--- src/app/heroes/[heroId]/page.tsx
+++ src/app/heroes/[heroId]/page.tsx
@@ -8,8 +8,21 @@ const HeroPage = () => {
   const { heroId } = useParams<{ heroId: string }>();
   const { data, isLoading } = useGetHeroProfileByIdQuery({ id: heroId });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (!data) return <div>No profile found.</div>;
+  if (isLoading)
+    return (
+      <p className="text-xl text-neutral-600 dark:text-neutral-300">
+        載入中...
+      </p>
+    );
+
+  if (!data)
+    return (
+      <p className="text-xl text-center tracking-widest leading-relaxed text-neutral-600 dark:text-neutral-300">
+        找不到這位英雄喔...
+        <br />
+        請選擇上面任何一位進行查看吧！
+      </p>
+    );
 
   return (
     <div className="w-full flex justify-center">
